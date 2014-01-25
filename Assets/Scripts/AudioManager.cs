bbb555d6f14d0affad6dea872bbf56d98fc6e36a
John Harger <john.harger@gmail.com>
2014-01-25 09:01:43 -0700

    Adding an audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : Singleton<AudioManager> {
+	public int numAudioSources = 32;
+	public AudioSource audioSourcePrefab;
+
+	private Queue<AudioSource> sourceQueue;
+
+	void Awake () {
+		GameObject.DontDestroyOnLoad(gameObject);
+
+		sourceQueue = new Queue<AudioSource>(numAudioSources);
+		for(int i = 0; i < numAudioSources; i ++) {
+			AudioSource source = (AudioSource)Instantiate(audioSourcePrefab);
+			source.transform.parent = transform;
+			sourceQueue.Enqueue(source);
+		}
+	}
+
+	public void PlayAudioClip(AudioClip clip)
+	{
+		AudioSource source = sourceQueue.Dequeue();
+		source.PlayOneShot(clip);
+		sourceQueue.Enqueue(source);
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+	static T _instance;
+	
+	public static T instance {
+		get {
+			if(!_instance) {
+				_instance = (T)FindObjectOfType(typeof(T));
+			}
+			return _instance;
+		}
+	}
+}

4e23ef4eec88ca8b6e1ce3ca49b6132e88ff7d9e
John Harger <john.harger@gmail.com>
2014-01-26 16:45:37 -0700

    The boss

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+	public float damage = 26f;
+	public float lifeTime = 10f;
+	float countdown = 0f;
+
+	void Start()
+	{
+		countdown = lifeTime;
+	}
+
+	void Update()
+	{
+		if(countdown > 0f) {
+			countdown -= Time.deltaTime;
+			if(countdown <= 0f) {
+				particleSystem.enableEmission = false;
+			}
+		}
+
+		if(particleSystem.particleCount < 0) {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if(countdown <= 0f) {
+			return;
+		}
+
+		other.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+		Destroy(gameObject, 1f);
+		countdown = 0f;
+	}
+}

04c58bb4b0e6f464e059fd21ba2f52980c671716
John Harger <john.harger@gmail.com>
2014-01-26 15:26:40 -0700

    Updated level 1 to reset scores

---
+++ Assets/Scripts/ScoreReset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreReset : MonoBehaviour {
+	void Awake () {
+		PlayerPrefs.SetInt("playerLoveLevel", 0);
+		PlayerPrefs.SetInt("playerGreedLevel", 0);
+		PlayerPrefs.SetInt("playerSmartLevel", 0);
+	}
+	
+}

9a670d88d64f0b1505bfc7a569167d42b33b6d10
kurtdog <kurtdog@unm.edu>
2014-01-26 12:04:00 -0700

    Vending Machine and Fire

---
+++ Assets/Scripts/FireScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireScript : MonoBehaviour {
+
+	ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+	
+		ps = GetComponent<ParticleSystem>();
+		ps.renderer.sortingLayerName("Foreground");
+		burnShit();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void burnShit()
+	{
+		ps.Play();
+	}
+}
